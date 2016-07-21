149d4346952365df1cb3aa1275c8c979e4faf4a2
Wesobi <wesley@wesobi.be>
2016-07-21 17:15:37 +0200

    [UPDATE] - changed gulpfile

--- www/app/Resources/gulpfile.js
+++ www/app/Resources/gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
 var sass = sass = require('gulp-sass');
 
+var paths = {
+    sass: 'styles/**/*.scss'
+};
 
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function () {
     return gulp.src('styles/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('../../web/css/main.css'));
+        .pipe(gulp.dest('../../web/css'));
 });
 
 gulp.task('watch', function() {
